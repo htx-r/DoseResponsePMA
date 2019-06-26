9c733e702c55471678059a7c526315c4e45afb12
Tasnim <tasneemhamza1989@gmail.com>
2019-06-26 10:25:39 +0200

    update

--- runSimulatedDRmetaFreq.R
+++ runSimulatedDRmetaFreq.R
@@ -9,7 +9,7 @@ library(DoseResponseNMA)
 
 # compare dosresmeta vs true values
 m1 <- m2 <- f1<-f2 <- vector()
-n.sim.data <- 100
+n.sim.data <- 1000
 ns <- 20
 start <- Sys.time()
 
@@ -43,7 +43,7 @@ mres2 <- mean(m2)
 
 # compare dosresmeta vs true values
 f1 <- vector()
-n.sim.data <- 10
+n.sim.data <- 1000
 for (j in 1:n.sim.data) {
 
   sim.data <- simulateDRlineardata.fun(beta.pooled=0.02,tau=0.001)
@@ -62,7 +62,7 @@ bias<- 0.02-resf ## I changed beta.pooled several times and normally I get 0.03 
 
 ## Quadratic
 m1 <- m2 <-f1<-f2 <- vector()
-n.sim.data <- 100
+n.sim.data <- 1000
 for (i in 1:n.sim.data) {
 
   sim.data <- simulateDRquadraticdata.fun(beta1.pooled=0.01,beta2.pooled=0.02,tau=0.001,doserange = c(1,10))
@@ -72,8 +72,8 @@ for (i in 1:n.sim.data) {
    #                              se = selogRR, cases = cases, n = cases+noncases, data = sim.data, proc='1stage',method = 'fixed')#!!!!!!!!!!!!!!
   m <- lm(formula = logRR ~ dose+I(dose^2) - 1, data = sim.data)
 
-  #f1[j] <-coef(quadraticDRmetaFreq)[1]
-  #f2[j] <-coef(quadraticDRmetaFreq)[2]
+  f1[j] <-coef(quadraticDRmetaFreq)[1]
+  f2[j] <-coef(quadraticDRmetaFreq)[2]
 
   m1[j] <-coef(m)[1]
 
